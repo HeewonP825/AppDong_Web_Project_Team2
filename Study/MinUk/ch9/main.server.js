d4c5133b71d0d2973f1f027d25e6936447140610
J-MU <alsdnrdl001@gmail.com>
2022-05-24 15:06:34 +0900

    node.js - mysql 연동 공부중

--- Study/MinUk/ch9/main.server.js
+++ Study/MinUk/ch9/main.server.js
@@ -21,31 +21,43 @@ app.listen(9999,function(){
 });
 
 app.get('/',function(request,response){
-    fs.readFile('list.html','utf8',function(error,data){
-        client.query('SELECT * FROM PRODUCTS',function(error,result){
+    fs.readFile('./list.html','utf8',function(error,data){
+        client.query('SELECT * FROM PRODUCTS',function(error,results){
             response.send(ejs.render(data,{
                 data:results
             }));
+
+            console.log(results);
         });
     });
 });
 
 app.get('/delete/:id',function(request,response){
-
+    console.dir(request);
+    client.query('DELETE FROM PRODUCTS WHERE id=?',[request.params.id],function(){
+        response.redirect('/');
+    })
 });
 
 app.get('/insert',function(request,response){
-
+    fs.readFile('insert.html','utf-8',function(error,data){
+        response.send(data);
+    });
 });
 
 app.post('/insert',function(request,response){
+    // console.dir(request.body);
+    var body=request.body;
 
+    client.query('INSERT INTO PRODUCTS (name,modelnumber,series) VALUES(?,?,?)',[body.name,body.modelnumber,body.series],function(){
+        response.redirect('/');
+    })
 });
 
-app.get('/edit/:id',function(request,response){
+// app.get('/edit/:id',function(request,response){
 
-});
+// });
 
-app.post('/edit/:id',function(request,response){
+// app.post('/edit/:id',function(request,response){
 
-});
+// });
