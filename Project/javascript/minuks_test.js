2fa865f591d82295d4a5d68ec681c3f3975f6205
J-MU <alsdnrdl001@gmail.com>
2022-05-31 01:02:21 +0900

    no message

--- Project/javascript/minuks_test.js
+++ Project/javascript/minuks_test.js
@@ -12,4 +12,18 @@ function getAPI(){
         });
 }
 
-getAPI();
+// event listener page load -> getAPI라는 함수를 실행시켜라
+
+window.addEventListener('load',function(){
+    console.log('load event가 감지되었습니다.')
+    const url=`http://127.0.0.1:9999/store/restaurant_info`;
+    fetch(url)
+        .then(response=>{
+            console.log(response);
+            response.json();
+        })
+        .then(data=>{
+            console.log(data);
+        });
+});
+
