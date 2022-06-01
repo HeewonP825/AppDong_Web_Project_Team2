12ec2ce3a5561e58fd012487c9f57a0c786892cd
J-MU <alsdnrdl001@gmail.com>
2022-06-01 21:14:30 +0900

    restAPI구현 토요일쯤엔 진짜 올릴께요

--- Project/server/main.js
+++ Project/server/main.js
@@ -3,6 +3,9 @@ var express=require('express');
 var fs=require('fs');
 var mysql=require('mysql');
 var path=require('path');
+var url=require('url');
+var querystring=require('querystring');
+const { checkPrime } = require('crypto');
 
 var app=express();
 // 정적 파일 로드하기.
@@ -61,26 +64,28 @@ app.get('/store',function(request,response){
 
 //store페이지를 get으로 받아오면 조건에 맞게 database set을 보내준다.
 app.get('/store/restaurant_info',function(request,response){
-    client.query('SELECT * FROM RESTAURANT_INFO',function(error,result,fields){
-        if(error){
-            console.log('쿼리 문장에 오류가 있습니다.');
-        }else{
-            response.json(result);
-            console.log('데이터 보냈음^^');
-        }
-    });
-    console.log('정상 데이터 송신 완료');
+    const queryObj=request.query;
+    if(queryObj===Object && Object.key(queryObj).length===0){
+        client.query('SELECT * FROM RESTAURANT_INFO',function(error,result,fields){
+            if(error){
+                console.log('쿼리 문장에 오류가 있습니다.');
+                return;
+            }
+            checkQueryParameter(queryObj);
+        });
+        console.log('정상 데이터 송신 완료');
+    }else{
+        console.log(request.query);
+    }
 });
 
-app.get('/Project/html/minuk_test.html',function(request,response){
-    fs.readFile('../html/minuk_test.html','utf-8',function(error,data){
-        if(error){
-            console.log('Loading minuk_test.html is failed');
-        }
-        response.send(data);
-    })
-})
+function checkQueryParameter(queryObj){
+    const valid_param=['restaurant_type','wine','beer','delivery','kids_room','name'];
 
+    for(param in queryObj.key){
+        
+    }
+}
 
 app.get('/my-route', function(request, response, next) {
 
@@ -92,3 +97,18 @@ app.get('/my-route', function(request, response, next) {
         }
     });
 });
+
+
+
+
+
+
+
+// app.get('/Project/html/minuk_test.html',function(request,response){
+//     fs.readFile('../html/minuk_test.html','utf-8',function(error,data){
+//         if(error){
+//             console.log('Loading minuk_test.html is failed');
+//         }
+//         response.send(data);
+//     })
+// })
